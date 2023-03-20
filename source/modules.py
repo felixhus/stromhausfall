859fad870c1a858afe2ca8a420a1806e14800577
Fusebot <felix@husemann.de>
2023-03-20 13:29:22 +0100

    Import sql angepasst

--- source/modules.py
+++ source/modules.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
-import sql_modules
 
 import source.grid_objects as grid_objects
 import source.objects as objects
+import source.sql_modules as sql_modules
 
 
 def get_last_id(elements):
